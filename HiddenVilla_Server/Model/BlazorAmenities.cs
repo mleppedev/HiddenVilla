3f25627fe4a6dd9c56f87ddf443fdfd2eb06af39
mleppedev <mleppedev@gmail.com>
2021-11-05 22:32:50 -0300

    Fin Tarea 4

---
+++ HiddenVilla_Server/Model/BlazorAmenities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorAmenities
+    {
+        //tarea 04 - 01 - se crean las propiedades de las amenities o comodidades
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
